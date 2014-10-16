2740010d2e3da6f467293efc378b580f4e4a29ed
Saad Sawari <saad1200@gmail.com>
2014-10-16 01:28:40 +0100

    added articles to leftsidebar

--- src/app/layout/leftSidebar.js
+++ src/app/layout/leftSidebar.js
@@ -1,14 +1,28 @@
 (function(){
 
-    angular.module('app').controller('leftSidebar', ['writersService', leftSideBar]);
+    angular.module('app').controller('leftSidebar', ['writersService', 'preloaderImageService', leftSideBar]);
 
-    function leftSideBar(writersService){
+    function leftSideBar(writersService, preloaderImageService){
 
         var vm = this;
         vm.writers = [];
+        vm.writersArticles = [];
 
         writersService.getLatest().success(function(results){
             vm.writers = results;
+        });
+
+        writersService.getArticles().then(function (result) {
+            var imagesToPreload = [];
+            for(var index in result.data)
+                imagesToPreload.push(result.data[index].pictures);
+
+            preloaderImageService.preloadImages( imagesToPreload ).then(
+                function handleResolve( imageLocations ) {
+                    vm.writersArticles = result.data;
+                });
+
+            return vm.articles ;
         });
     }
 })();
@@ -22,6 +36,10 @@
         this.getLatest = function(){
           return $http.get('http://www.almogtarbeen.com/writers/getLatest?size=5q');
         };
+
+        this.getArticles = function(){
+            return $http.get('http://www.almogtarbeen.com/articles/getArticles?size=8&categories=(17,5)');
+        }
     }
 
 })();
