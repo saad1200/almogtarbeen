683617092265a1e249daf9ab72bcedd107b53acc
Saad Sawari <saad1200@gmail.com>
2014-10-19 23:00:26 +0100

    use one main layout

--- src/app/layout/leftSidebar.js
+++ src/app/layout/leftSidebar.js
@@ -1,40 +1,25 @@
 (function(){
     'use strict'
 
-    angular.module('app').controller('leftSidebar', ['writersService', 'broadcaster', leftSideBar]);
+    angular.module('app').controller('leftSidebar', ['$scope', 'broadcaster', leftSideBar]);
 
-    function leftSideBar(writersService, broadcaster){
+    function leftSideBar($scope, broadcaster){
 
         var vm = this;
-//        vm.writers = [];
-//        vm.writersArticles = [];
         vm.threeView = [];
         vm.highlightView = [];
         vm.listView = [];
 
-        writersService.getLatest().success(function(results){
-            vm.writers = results;
-        });
-
-        broadcaster.onUpdateArticlesViews(function(event, data){
+        var removeOnUpdateArticlesViews = broadcaster.onUpdateArticlesViews(function(event, data){
             while (data.length > 0)
                 vm.threeView.push(data.splice(0, 4));
-//            vm.threeView.push(data);
-//            vm.highlightView = data.slice(15, 16);
-//            vm.listView = data.slice(15, 20);
+                vm.threeView.push(data.splice(4, 8));
+                vm.threeView.push(data.splice(8, 12));
         });
-//        writersService.getArticles().then(function (result) {
-//            var imagesToPreload = [];
-//            for(var index in result.data)
-//                imagesToPreload.push(result.data[index].pictures);
-//
-//            preloaderImageService.preloadImages( imagesToPreload ).then(
-//                function handleResolve( imageLocations ) {
-//                    vm.writersArticles = result.data;
-//                });
-//
-//            return vm.articles ;
-//        });
+
+        $scope.$on('$destroy', function(){
+            removeOnUpdateArticlesViews();
+        });
     }
 })();
 
